00b6c39c440efa9329a886d27b39ec9cd0cf4fb9
Laryssa Souza <laarylaryy@gmail.com>
2023-04-06 14:59:14 -0300

    exerc04

--- lista.py
+++ lista.py
@@ -3,3 +3,16 @@ print(exerc01)
 
 exerc02 = [10,9,8,7,6,5,4,3,2,1]
 print(exerc02)
+
+exerc03 = [10,7.4,8.2,9.0]
+media = 0
+for i in range(0,len(exerc03)):
+    print("nota", i+1, ":",exerc03[i])
+    media = media + exerc03[i]
+media = media / len(exerc03)
+print("media:", media)
+
+exerc04 = ["a","b","c","d","e","f","g","h","i","j"]
+for i in exerc04:
+    if i not in "a,e,i,o,u":
+        print(i)
