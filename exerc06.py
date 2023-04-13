9135b04cc76764a58c889a2f06b53fec3a6bced3
Laryssa Souza <laarylaryy@gmail.com>
2023-04-13 11:11:40 -0300

    exerc006

--- exerc06.py
+++ exerc06.py
@@ -1,16 +1,24 @@
-media = []
-notas = []
-alunos_aprovados = 0
+aluno1 = [5,7,7,8]
+aluno2 = [6,8,8,9]
+aluno3 = [10,9,8,9]
+aluno4 = [5,5,4,6]
+aluno5 = [3,6,7,8]
+aluno6 = [10,10,9,8]
+aluno7 = [5,6,7,8]
+aluno8 = [8,9,9,8]
+aluno9 = [10,7,8,8]
+aluno10 = [8,8,9,10]
+alunos = [aluno1,aluno2,aluno3,aluno4,aluno5,aluno6,aluno7,aluno8,aluno9,aluno10]
+contagem = 0
+final = ""
+media = 0
 
-for i in range(10):
-    print(f"notas do aluno{i+1}:")
-    nota1 = float(input("Digite a primeira nota:“"))
-    nota2 = float(input("Digite a segunda nota: "))
-    nota3 = float (input ("Digite a terceira nota: "))
-    nota4 = float (input ("Digite a quarta nota: "))
-    notas.append([nota1, nota2, nota3, nota4])
-    media_aluno = (nota1 + nota2 + nota3 + nota4) / 4
-    media.append(media_aluno)
-    if media_aluno >=7.0:
-        alunos_aprovados += 1
-print (f"número de alunos com média maior ou igual a 7.0: (alunos _aprovados]")
+for i in alunos:
+    for nota in alunos[contagem]:
+        media = media + nota
+        media = media / len(alunos[contagem])
+        if media >= 7:
+            final = f"{final} Aluno {contagem+1} (Aprovado) - {media} |"
+            contagem +=1
+            media = 0
+            print("medias:", final)
